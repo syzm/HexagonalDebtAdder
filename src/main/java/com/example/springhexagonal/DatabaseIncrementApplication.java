f1e392146c3b2ccd81ac25de216572e3fcb68f23
syzm <szymone25@gmail.com>
2024-02-18 22:29:02 +0000

    Initial commit

---
+++ src/main/java/com/example/springhexagonal/DatabaseIncrementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseIncrementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseIncrementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springhexagonal/DatabaseIncrementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseIncrementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
